fffbdcb7eb689b18b609d6f17cbf50afe6a1335c
Federico Simonetti <federicosimonetti94@gmail.com>
2016-05-18 02:33:43 +0200

    Ho iniziato a strutturare il model ma ancora le funzionalità non sono state implementate

--- application/controllers/PublicController.php
+++ application/controllers/PublicController.php
@@ -2,115 +2,57 @@
 
 class PublicController extends Zend_Controller_Action
 {
-	protected $_descprod = '<p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Phasellus pellentesque. Mauris quam enim, molestie in, rhoncus ut, lobortis a, est. </p><p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Phasellus pellentesque. Mauris quam enim, molestie in, rhoncus ut, lobortis a, est.</p>';
-	protected $_logger;
+	protected $_catalogModel;
 	
     public function init()
     {
 		$this->_helper->layout->setLayout('main');
-		$this->_logger = Zend_Registry::get('log');  	
+        $this->_catalogModel = new Application_Model_Catalog();
     }
 
     public function indexAction()
     {    	    	
-    	//  Categorie Top    	    	
-		$topCats=array((object) array('catId'=>'1',
-							 		  'name'=>'Computer',
-									  'parId'=>'0',
-									  'desc'=>'Desktop, Laptop, Netbook'),
-					   (object) array('catId'=>'2',
-							 		  'name'=>'Periferiche',
-									  'parId'=>'0',
-					   				  'desc'=>'Hard Disk, Tastiere, Mouse')
-					   );
-					   
-    	//  Sottocategorie    	    	
+    	//  Estrae le Categorie Top    	    	
+    	$topCats=$this->_catalogModel->getTopCats();
+
+    	//  Estrae le Sottocategorie    	    	
 		$topId = $this->_getParam('selTopCat', null);
 		if (!is_null($topId)) {
-
-		// Debug
-		$this->_logger->info('Attivato ' . __METHOD__ . ' - Valore di $topCats[0]->name = ' . $topCats[$topId-1]->name);
-
-        	switch ($topId) {
-            	case '1':
-					$subCats=array((object) array('catId'=>'3',
-							 		  			  'name'=>'Desktop',
-												  'parId'=>'1',
-									  			  'desc'=>'Descrizione dei Prodotti Desktop'),
-					   			   (object) array('catId' => '4',
-							 		  			  'name'=>'Laptop',
-									  			  'parId'=>'1',
-					   			   				  'desc'=>'Descrizione dei Prodotti Laptop'),
-					   			   (object) array('catId' => '5',
-							 		  			  'name'=>'NetBook',
-									  			  'parId'=>'1',
-					   			   				  'desc'=>'Descrizione dei Prodotti NetBook')
-					   			   );
-					break;
-            	case '2':
-					$subCats=array((object) array('catId' => '6',
-							 		  			  'name'=>'HardDisk',
-									  			  'parId'=>'2',
-												  'desc'=>'Descrizione dei Dischi Rigidi'),
-					   			   );
-					break;
-        	}
+    		$subCats=$this->_catalogModel->getCatsByParId($topId);
 		} else {
 			$subCats = null;
 		}
 
-		// Prodotti
-		$prods=array();
+		// Estrae i Prodotti
 		$cat = $this->_getParam('selCat', null);
+		$paged = $this->_getParam('page', 1);
 		if (!is_null($cat)) {
-        	switch ($cat) {
-            	case '3':
-					$prods=array((object) array('prodId'=>'1',
-							 		  			  'name'=>'Desktop Modello1',
-												  'catId'=>'3',
-									  			  'descShort'=>'Caratteristiche Desktop1',
-												  'descLong'=> $this->_descprod,
-												  'price'=>'1230.49',
-												  'discountPerc'=>'25',
-												  'discounted'=>'1',
-												  'image'=>'Italy.gif'),
-					   			   );
-					break;
-            	case '4':
-					$prods=array((object) array('prodId'=>'2',
-							 		  			  'name'=>'Laptop Modello1',
-												  'catId'=>'4',
-									  			  'descShort'=>'Caratteristiche Laptop1',
-												  'descLong'=> $this->_descprod,
-												  'price'=>'445.37',
-												  'discountPerc'=>'17',
-												  'discounted'=>'0',
-												  'image'=>''),
-					
-					   			 (object) array('prodId'=>'3',
-							 		  			  'name'=>'Laptop Modello2',
-												  'catId'=>'4',
-									  			  'descShort'=>'Caratteristiche Laptop2',
-												  'descLong'=> $this->_descprod,
-												  'price'=>'1889.67',
-												  'discountPerc'=>'15',
-												  'discounted'=>'1',
-												  'image'=>'Argentina.gif'),
-					   			 );
-					break;
-            }
-		}  
-			   
+			
+		//	Categoria selezionata: estrae i prodotti
+			$prods=$this->_catalogModel->getProdsByCat($cat, $paged, $order=null, $deep=false);
+		} elseif (!is_null($topId)){
+			
+		//	TopCat selezionata: Estrae i prodotti in sconto
+			$prods=$this->_catalogModel->getDiscProds($topId, $paged, $order=array('discountPerc DESC'), $deep=true); 
+		} else {
+			
+		//	Nessuna selezione: estrae tutti i prodotti in sconto
+			foreach ($topCats as $topCat) {
+				$topCatsList[] = $topCat->catId;
+			}
+			$prods=$this->_catalogModel->getDiscProds($topCatsList, $paged, $order=array('discountPerc DESC'), $deep=true);			   	
+		}
+		  		   
     	// Definisce le variabili per il viewer
     	$this->view->assign(array(
             		'topCategories' => $topCats,
-    				'selectedTopCat' => (is_null($topId) ? null : $topCats[$topId-1]->name),
-    				'subCategories' => $subCats,
+    				'selectedTopCat' => (is_null($topId) ? null : $this->_catalogModel->getCatById($topId)->name),
+            		'subCategories' => $subCats,
             		'products' => $prods
-    							)
+            		)
         );
     }
-
+ 	
     public function viewstaticAction () {
     	$page = $this->_getParam('staticPage');
     	$this->render($page);
