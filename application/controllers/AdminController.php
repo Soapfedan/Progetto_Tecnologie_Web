343eee178c483b4651f492ca51f6185309f3d9f3
Ale Bon <Utente1@172.23.154.245>
2016-05-28 16:07:22 +0200

    xbn

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -5,6 +5,7 @@ class AdminController extends Zend_Controller_Action
     public function init()
     {
         $this->_helper->layout->setLayout('arear');
+        $this->_adminModel = new Application_Model_Admin();
        
     }
     public function indexAction()
@@ -20,7 +21,13 @@ class AdminController extends Zend_Controller_Action
     }
     
     public function userAction(){
-        $this->view->msg='user';  
+        $this->view->msg='user'; 
+        
+        //  Estrae tutti gli utenti registrati al sito.               
+        $datauser = $this->_adminModel->getAllUsers();
+                 
+        // Definisce le variabili per il viewer
+        $this->view->assign(array('users' => $datauser)); 
     }
     
     public function immAction(){
