3f5bb0c3b7d1c077acc52866ebe0befeb96ae727
Federico Simonetti <federicosimonetti94@gmail.com>
2016-05-28 16:51:59 +0200

    Messa la form di modifica profilo

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -7,6 +7,7 @@ class UserController extends Zend_Controller_Action
     
     public function init()
     {
+        
 		$this->_userModel = new Application_Model_User();  
          $this->_helper->layout->setLayout('arear');
          $this->_authService = new Application_Service_Authentication();
@@ -39,13 +40,53 @@ class UserController extends Zend_Controller_Action
     }
     
      public function editprofileAction(){
-         
+        
         $this->view->msg = 'editProfile';
+        $completed = $this->_getParam('staticPage');
+        $filled = $this->_getParam('staticPage');
+        $username = $this->_getParam('staticPage');
+        $this->view->profileForm = $this->getProfileForm(($completed==false ? false : true),
+                                                         ($filled==false ? false : true),
+                                                         ($username==null ? null : $username)); 
+        
+        
     }
      
       public function viewescapeplanAction(){
           
         $this->view->msg = 'viewEscapePlan';
     } 
+      
+       private function getProfileForm($completed,$filled,$username)
+    {
+        $identity = $this->_authService->getIdentity();
+        if($identity!=false){
+            $controller='user';
+         switch ($identity->Categoria) {
+            case 1:
+               $controller='user'; 
+                break;
+             case 2:
+                $controller='staff';
+                break;
+                  case 3:
+                $controller='admin';
+                break;
+            default:
+                
+                break;
+        }
+            
+        $urlHelper = $this->_helper->getHelper('url');
+        $this->_form = new Application_Form_User_Profilo_Profile();
+        $this->_form->setAction($urlHelper->url(array(
+                        'controller' => $controller,
+                        'action'     => 'index'
+                        ), 
+                        'default',true
+                    ));
+        return $this->_form;
+    }
+    }
    
 }
---
+++ application/forms/User/Profilo/Profile.php
@@ -0,0 +1,135 @@
+<?php
+class Application_Form_User_Profilo_Profile extends App_Form_Abstract
+{
+	protected $_userModel;
+    
+    public function init($completed,$filled,$username)
+    {
+    	$this->_userModel = new Application_Model_User();
+        $this->setMethod('post');
+        $this->setName('formprofile');
+        $this->setAction('');
+        
+     if($complete){ 
+       $this->addElement('text', 'Username', array(
+            'label' => 'Username',
+            'filters' => array('StringTrim'),
+            'required' => true,
+            'validators' => array(array('StringLength',true, array(5,30))),
+            'decorators' => $this->elementDecorators,
+        ));
+        
+      $this->addElement('password', 'Password', array(
+            'label' => 'Password',
+            'filters' => array('StringTrim'),
+            'required' => true,
+            'validators' => array(array('StringLength',true, array(5,30))),
+            'decorators' => $this->elementDecorators,
+        ));
+     } 
+      $this->addElement('text', 'Nome', array(
+            'label' => 'Nome',
+            'filters' => array('StringTrim'),
+            'required' => true,
+            'validators' => array(array('StringLength',true, array(5,30))),
+            'decorators' => $this->elementDecorators,
+        ));
+
+        $this->addElement('text', 'Cognome', array(
+            'label' => 'Cognome',
+            'filters' => array('StringTrim'),
+            'required' => true,
+            'validators' => array(array('StringLength',true, array(5,30))),
+            'decorators' => $this->elementDecorators,
+        ));
+        
+         $this->addElement('text', 'Data_di_Nascita', array(
+            'label' => 'Data di nascita',
+            'format' => 'Y-m-d\TH:iP',
+            'required' => true,
+            'validators' => array(array('StringLength',true, array(1,25))),
+            'decorators' => $this->elementDecorators,
+        ));
+        
+       $this->addElement('text', 'Citta', array(
+            'label' => 'Città',
+            'filters' => array('StringTrim'),
+            'required' => true,
+            'validators' => array(array('StringLength',true, array(1,20))),
+            'decorators' => $this->elementDecorators,
+        ));
+        
+        $this->addElement('text', 'Provincia', array(
+            'label' => 'Provincia',
+            'filters' => array('StringTrim'),
+            'required' => true,
+            'validators' => array(array('StringLength',true, array(2))),
+            'decorators' => $this->elementDecorators,
+        ));
+        
+       $this->addElement('select', 'Genere', array(
+            'label' => 'Genere',
+            'required' => true,
+            'multiOptions' => array('M' => 'Maschio', 'F' => 'Femmina'),
+            'decorators' => $this->elementDecorators,
+        ));
+        
+        
+        $this->addElement('text', 'Codice_fiscale', array(
+            'label' => 'Codice Fiscale',
+            'filters' => array('StringTrim'),
+            'required' => true,
+            'validators' => array(array('StringLength',true, array(16))),
+            'decorators' => $this->elementDecorators,
+        ));
+        
+        $this->addElement('text', 'Email', array(
+            'label' => 'Email',
+            'filters' => array('StringTrim'),
+            'required' => true,
+            'validators' => array('EmailAddress'),
+            'decorators' => $this->elementDecorators,
+        ));
+        
+       $this->addElement('text', 'Telefono', array(
+            'label' => 'Telefono',
+            'filters' => array('Digits'),
+            'required' => true,
+            'validators' => array(array('StringLength',true, array(15))),
+            'decorators' => $this->elementDecorators,
+        ));
+        if($complete){
+        $this->addElement('select', 'Categoria', array(
+            'label' => 'Categoria',            
+            'required' => true,
+            'multiOptions' => array('1' => 'Utente', '2' => 'Staff','3' => 'Amministratore'),
+            'decorators' => $this->elementDecorators,
+        ));
+        
+       $this->addElement('text', 'Societa_staff', array(
+            'label' => 'Società Staff',
+            'filters' => array('Digits'),
+            'required' => true,
+            'validators' => array(array('StringLength',true, array(1))),
+            'decorators' => $this->elementDecorators,
+        ));
+        }
+
+        if($filled){
+            $values = array();
+            $values = $this->_userModel->getUserInformation($username);
+            $this->populate($values->toArray());
+        }
+        $this->addElement('submit', 'add', array(
+            'label' => 'Invia dati',
+        	'decorators' => $this->buttonDecorators,
+        ));
+        
+       $this->setDecorators(array(
+            'FormElements',
+            array('HtmlTag', array('tag' => 'table')),
+        	array('Description', array('placement' => 'prepend', 'class' => 'formerror')),
+            'Form'
+        ));
+    }
+}
