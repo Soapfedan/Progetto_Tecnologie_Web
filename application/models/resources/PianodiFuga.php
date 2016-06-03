0702256b9df47f233cca060e4553b4619d3ba283
Federico Simonetti <federicosimonetti94@gmail.com>
2016-06-03 15:25:38 +0200

    dfsdf

--- application/models/resources/PianodiFuga.php
+++ application/models/resources/PianodiFuga.php
@@ -12,20 +12,17 @@ class Application_Resource_PianodiFuga extends Zend_Db_Table_Abstract
     //estrae il piano di fuga relativo ad una zona e ad un piano. Se è stato inserito
     //dallo staff un percorso alternativo sarà visuallizato tale percorso altrimenti quello 
     //di default
-    public function getEscapePlan($zone,$floor){
+    public function getEscapePlan($zone,$floor,$imm){
         
          $select = $this->select()
                         ->from(array('p' => 'piano_di_fuga'),
                          array('Piano_di_fuga', 'Piano_di_fuga_alternativo'))
                         ->where('Zona =?',$zone)
-                        ->where('Id_piano =?',$floor);
+                        ->where('Id_piano =?',$floor)
+                        ->where('Immobile =?',$imm);
         $result= $this->fetchRow($select);
-        $escapeplan=$result['Piano_di_fuga'];
-        if($result['Piano_di_fuga_alternativo']!=null){
-            $escapeplan=$result['Piano_di_fuga_alternativo'];
-        }
-        
-         return $escapeplan;   
+         
+         return $result;   
             
     }
     
