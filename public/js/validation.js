1d05f8a5d700c6a06038c2471e2e5321b95c923d
Ale Bon <Utente1@172.23.154.245>
2016-06-04 18:30:24 +0200

    hr

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,10 +1,8 @@
 function check(){
 	
-	docuemnt.writeln('aaa');
 	if($('#Password').val() != $('#Password1').val()){
 		$('#Password').addClass('wrong');
 	}
 	else
 		$('#Password').removeClass('wrong');
-	docuemnt.writeln('bbb');
 }
