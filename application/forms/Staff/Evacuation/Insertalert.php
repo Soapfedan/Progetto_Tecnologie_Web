544b171095acf57e645a2f280d90ab3c958cbba6
simone Accattoli <accattoli.simone@gmail.com>
2016-06-06 14:08:55 +0200

    coijioa

--- application/forms/Staff/Evacuation/Insertalert.php
+++ application/forms/Staff/Evacuation/Insertalert.php
@@ -4,34 +4,59 @@ class Application_Form_Staff_Evacuation_Insertalert extends App_Form_Abstract
    
         
     public function init(){
+	}
            
-        
+    public function create($immobile, $piano,$valuesZone,$valuesDisaster){        
         $this->setMethod('post');
-        $this->setName('insertalertform');
+        $this->setName('sendalertform');
         $this->setAction('');
+		
+			/*
+			$optionsZone = array();
+			$optionsDisaster = array();  
+			$i=1;
+			foreach ($valuesZone as $zone){
+				$optionsZone[$i] = $zone['Zona'];
+				$i++;
+			}
+			$i=1;
+			foreach ($valuesDisaster as $disaster){
+				$optionsDisaster[$i] = $disaster['Descrizione'];
+				$i++;
+			}
+			$select = new Zend_Form_Element_Select('Codice_Zona');
+			$select->setMultiOptions($optionsZone);
+			$select->setLabel('Zona da segnalare');
+			$selectDisaster = new Zend_Form_Element_Select('Tipo_Catastrofe');
+			$selectDisaster->setMultiOptions($optionsDisaster);
+			$selectDisaster->setLabel('Evento da segnalare');
             
+            $this->addElement($select);
+			$this->addElement($selectDisaster);
+			
+        
+            $this->addElement('submit','Inserisci',array(
+                    'label' => 'Inserisci',
+                    'decorators' => $this->buttonDecorators,
+                ));
             
-         /*   
-        $values = $this->_adminModel->getAllBuildings();
-        $valuearr = $values->toArray();
-            // Crea un radioButton
-        $radio = new Zend_Form_Element_Radio('imms');
-            // Cicla sulla lista di immobili e aggiunge l'opzione relativa al radioButton
-        foreach($valuearr as $imm){
-            $radio->addMultiOption($imm['Immobile'], 'Immobile '.$imm['Immobile']);
-        }
-            // Aggiunge il radioButton finale alla form
-        $this->addElement($radio);
-                
-        $this->addElement('submit','modifica', array(
-            'label' => 'modifica',
-            'decorators' => $this->buttonDecorators,
-        ));
-        $this->addElement('submit','elimina', array(
-            'label' => 'elimina',
-            'decorators' => $this->buttonDecorators,
-        ));   */
-    }
+           $this->setAction($this->getView()->url(array(
+                    'controller' => 'staff',
+                    'action'     => 'insertalert',
+                    'immobile'   => $immobile,
+                    'floor'      => $piano
+                    ), 
+                    'default',true
+                ));
+		
+       $this->setDecorators(array(
+           'FormElements',
+           array('HtmlTag', array('tag' => 'table')),
+           array('Description', array('placement' => 'prepend', 'class' => 'formerror')),
+           'Form'
+       ));
+	   */
+       } 
 
 
 }
